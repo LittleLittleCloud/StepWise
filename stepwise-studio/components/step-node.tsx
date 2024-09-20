daac20c2bfb0b9be5e1f5348bbe86f5a1bd12cd4
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-09-19 22:17:07 -0700

    update

--- stepwise-studio/components/step-node.tsx
+++ stepwise-studio/components/step-node.tsx
@@ -1,6 +1,6 @@
 import { StepDTO } from '@/stepwise-client';
 import React, { useEffect, useState } from 'react';
-import { Handle, Position, NodeProps } from 'reactflow';
+import { Handle, Position, NodeProps, useUpdateNodeInternals } from 'reactflow';
 import { Button, buttonVariants } from './ui/button';
 import { cn } from '@/lib/utils';
 import { CircleUserRound, Play, RotateCcw, SquareFunction } from 'lucide-react';
@@ -12,10 +12,35 @@ export interface StepNodeProps {
 }
 const StepNode: React.FC<NodeProps<StepNodeProps>> = (prop) => {
     const [data, setData] = useState<StepDTO>(prop.data.data);
+    const titleRef = React.useRef<HTMLDivElement>(null);
+    const updateNodeInternals = useUpdateNodeInternals();
+    const [sourceHandleTopOffset, setSourceHandleTopOffset] = useState<number>(0);
+    const [targetHandleTopOffsets, setTargetHandleTopOffsets] = useState<number[]>([]);
 
     useEffect(() => {
         setData(prop.data.data);
+        setTargetHandleTopOffsets(Array(data.dependencies?.length ?? 0).fill(0));
     }, []);
+    const dividerRef = React.useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (titleRef.current) {
+            setSourceHandleTopOffset(titleRef.current.offsetTop + titleRef.current.offsetHeight / 2);
+
+            updateNodeInternals(prop.id);
+        }
+    }, [titleRef.current]);
+
+    useEffect(() => {
+        if (dividerRef.current && data.dependencies && data.dependencies.length > 0) {
+            const offsets = data.dependencies?.map((_, index) => {
+                const height = dividerRef.current!.offsetHeight / data.dependencies!.length;
+                const offset = dividerRef.current!.offsetTop + index * height + height / 2 - 2; // 2 is the padding top.
+                return offset;
+            }) ?? [];
+            setTargetHandleTopOffsets(offsets);
+        }
+    }, [dividerRef.current, data.dependencies]);
 
     return (
         <div className="rounded-md shadow-md p-2 bg-background/50 group min-w-32">
@@ -30,51 +55,23 @@ const StepNode: React.FC<NodeProps<StepNodeProps>> = (prop) => {
                     className='m-0 p-0'
                     onClick={() => prop.data.onRunClick(data)}
                 >
-                    <Play/>
+                    <Play />
                 </Button>
                 <Button
                     variant={"outline"}
                     size={"xxsIcon"}
                     className='m-0 p-0'
                 >
-                    <RotateCcw/>
+                    <RotateCcw />
                 </Button>
             </div>
 
-            {/* set up source handles */}
-            {/* the source handles will be put to the left */}
-            {/* and the y will start from 50, and incremented by 20 */}
-            {
-                data.dependencies && data.dependencies.map((dep, index) => (
-                    <Handle
-                        key={index}
-                        type="target"
-                        position={Position.Left}
-                        // id = name-dep
-                        id={`${data.name}-${dep}`}
-                        className="w-2 h-2 border-none bg-blue-500"
-                        style={{ top: 48 + index * 20 }}
-                    />
-                ))
-            }
-            {/* set up target handles */}
-            {/* the target handles will be put to the right */}
-            {/* and the y will start from 30 */}
-            {
-                <Handle
-                    type="source"
-                    position={Position.Right}
-                    // id = name-variable
-                    id={`${data.name}`}
-                    className="w-2 h-2 border-none bg-green-500"
-                    style={{ top: 17 }}
-                />
-            }
             {data.name && (
                 <div
                     className='flex gap-1 items-center'
                 >
                     <div
+                        ref={titleRef}
                         className={cn(buttonVariants(
                             {
                                 variant: "outline",
@@ -86,6 +83,14 @@ const StepNode: React.FC<NodeProps<StepNodeProps>> = (prop) => {
                         <SquareFunction size={10} />
                     </div>
                     <h2 className="text-xs font-semibold text-nowrap">{data.name}</h2>
+                    <Handle
+                        type="source"
+                        position={Position.Right}
+                        // id = name-variable
+                        id={`${data.name}`}
+                        className="w-2 h-2 border-none bg-green-500"
+                        style={{ top: sourceHandleTopOffset }}
+                    />
                 </div>
             )}
 
@@ -93,6 +98,9 @@ const StepNode: React.FC<NodeProps<StepNodeProps>> = (prop) => {
             {data.dependencies && data.dependencies.length > 0 && (
                 <div>
                     <div className="border border-primary/40 my-2" />
+                    <div
+                        ref={dividerRef}
+                    >
                     {
                         data.dependencies.map((dep, index) => (
                             <div key={index} className="flex gap-1 items-center text-xs pb-1">
@@ -106,9 +114,19 @@ const StepNode: React.FC<NodeProps<StepNodeProps>> = (prop) => {
                                     <SquareFunction size={10} />
                                 </div>
                                 <span>{dep}</span>
+                                <Handle
+                                    key={index}
+                                    type="target"
+                                    position={Position.Left}
+                                    // id = name-dep
+                                    id={`${data.name}-${dep}`}
+                                    className="w-2 h-2 border-none bg-blue-500"
+                                    style={{ top: targetHandleTopOffsets[index] }}
+                                />
                             </div>
                         ))
                     }
+                    </div>
                 </div>
             )}
 
