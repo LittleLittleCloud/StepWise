f67ef455544df37d73e5f788d45421093e28c014
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-11-27 14:10:45 -0800

    format

--- stepwise-studio/pages/index.tsx
+++ stepwise-studio/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-import Sidebar from "@/components/sidebar";
+import StepWiseSidebar from "@/components/sidebar";
 import {
 	client,
 	getApiV1StepWiseControllerV1ListWorkflow,
@@ -31,6 +31,7 @@ import {
 	ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { Toaster } from "@/components/ui/sonner";
+import { useWorkflowStore } from "@/hooks/useWorkflow";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -53,136 +54,24 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export default function Home() {
-	const [selectedWorkflow, setSelectedWorkflow] = useState<
-		WorkflowData | undefined
-	>(undefined);
-	const [workflows, setWorkflows] = useState<WorkflowData[]>([]);
-	const [version, setVersion] = useState<string | null>(null);
+	const selectedWorkflow = useWorkflowStore(
+		(state) => state.selectedWorkflow,
+	);
+	const updateWorkflow = useWorkflowStore((state) => state.updateWorkflow);
+	const fetchWorkflows = useWorkflowStore((state) => state.fetchWorkflows);
 
 	useEffect(() => {
-		getApiV1StepWiseControllerV1ListWorkflow()
-			.then((res) => {
-				var workflows: WorkflowData[] = [];
-
-				for (const workflow of res.data ?? []) {
-					var nodes = workflow.steps?.map((step: StepDTO) => ({
-						id: step.name,
-						width: 200,
-						height: 200,
-						position: { x: 0, y: 0 },
-					})) as Node[];
-
-					var edges =
-						workflow.steps?.reduce((edges, step) => {
-							return edges.concat(
-								step.dependencies?.map((dep) => {
-									return {
-										id: `${step.name}-${dep}`,
-										source: dep,
-										target: step.name,
-										sourceHandle: dep,
-										targetHandle: step.name + "-" + dep,
-										style: { stroke: "#555" },
-										animated: true,
-									} as Edge;
-								}) ?? [],
-							);
-						}, [] as Edge[]) ?? [];
-					var layout = getLayoutedElements(nodes, edges);
-
-					workflows.push({
-						...workflow,
-						stepSizes: layout.nodes.reduce(
-							(acc, node) => {
-								acc[node.id] = undefined;
-								return acc;
-							},
-							{} as {
-								[key: string]:
-									| {
-											width: number;
-											height: number;
-									  }
-									| undefined;
-							},
-						),
-						stepPositions: layout.nodes.reduce(
-							(acc, node) => {
-								acc[node.id] = {
-									x: node.position.x,
-									y: node.position.y,
-								};
-								return acc;
-							},
-							{} as { [key: string]: { x: number; y: number } },
-						),
-						stepRuns: [] as StepRunDTO[],
-					} as WorkflowData);
-				}
-				setWorkflows(workflows);
-				var maps = new Map<string, StepRunDTO[]>();
-				res.data?.forEach((workflow) => {
-					if (workflow.name === null || workflow.name === undefined) {
-						return;
-					}
-					maps.set(workflow.name, []);
-				});
-
-				setSelectedWorkflow(workflows[0] ?? undefined);
-			})
-			.catch((err) => {
-				console.error("Error getting workflows: ", err);
-			});
-		getApiV1StepWiseControllerV1Version().then((res) => {
-			setVersion(res.data ?? "Unknown");
-		});
+		fetchWorkflows();
 	}, []);
-
-	useEffect(() => {
-		console.log("Selected workflow: ", selectedWorkflow);
-		// update workflow
-		setWorkflows((prev) => {
-			const newWorkflows = [...prev];
-			const index = newWorkflows.findIndex(
-				(workflow) => workflow.name === selectedWorkflow?.name,
-			);
-			if (index !== -1) {
-				newWorkflows[index] = selectedWorkflow!;
-			}
-			return newWorkflows;
-		});
-	}, [selectedWorkflow]);
-
-	const selectedWorkflowHandler = (workflow: WorkflowData) => {
-		setSelectedWorkflow((prev) => {
-			return workflows.find((w) => w.name === workflow.name);
-		});
-	};
 
 	return (
 		<div
 			className={`w-full flex bg-accent gap-5 min-h-screen ${geistSans} ${geistMono}`}
 		>
-			<Sidebar
-				user="Test"
-				version={version ?? "Unknown"}
-				workflows={workflows}
-				selectedWorkflow={selectedWorkflow}
-				onWorkflowSelect={selectedWorkflowHandler}
-			/>
 			<Workflow
 				dto={selectedWorkflow}
 				onWorkflowChange={(workflowData) =>
-					setWorkflows((prev) => {
-						const newWorkflows = [...prev];
-						const index = newWorkflows.findIndex(
-							(workflow) => workflow.name === workflowData.name,
-						);
-						if (index !== -1) {
-							newWorkflows[index] = workflowData;
-						}
-						return newWorkflows;
-					})
+					updateWorkflow(workflowData)
 				}
 			/>
 			<Toaster />
