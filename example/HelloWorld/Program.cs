48dec84d6e3c0fd54ea2b66fb622e6bce8f9543a
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-12-18 12:29:50 -0800

    update

--- example/HelloWorld/Program.cs
+++ example/HelloWorld/Program.cs
@@ -2,60 +2,110 @@
 // Program.cs
 
 // Create a web host running on 5123 port
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using System.Security.Claims;
 using Gallery;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using StepWise.Core;
+using StepWise.Core.Extension;
 using StepWise.Gallery;
 using StepWise.WebAPI;
 
+
+
 var stepwiseConfig = new StepWiseServiceConfiguration
 {
-    EnableAuth0Authentication = false,
+    EnableAuth0Authentication = true,
+    Auth0Domain = "dev-7obvli7fq57vx30r.us.auth0.com",
+    Auth0ClientId = "ok4Im5Rt4blubBzvDRbM4SUixpmEGi8F",
+    Auth0Audience = "https://stepwisegallery20241128154731.azurewebsites.net/",
 };
+var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+var assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? Environment.CurrentDirectory;
+var webRoot = Path.Combine(assemblyDirectory, "wwwroot");
+var option = new WebApplicationOptions
+{
+    WebRootPath = webRoot,
+    EnvironmentName = "Development",
+    Args = args,
+    ApplicationName = "StepWise",
+};
+var builder = WebApplication.CreateBuilder(option);
 
-var host = Host.CreateDefaultBuilder()
-    .UseEnvironment("Development")
-    .ConfigureWebHostDefaults(webBuilder =>
+builder.WebHost.UseStepWiseServer(stepwiseConfig);
+builder.WebHost.UseWebRoot(webRoot);
+builder.UseStepWiseServer(configuration: stepwiseConfig);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = $"https://{stepwiseConfig.Auth0Domain}";
+    options.Audience = stepwiseConfig.Auth0Audience;
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        webBuilder.UseUrls("http://localhost:5123");
-    })
-    .UseStepWiseServer(stepwiseConfig)
-    .Build();
+        NameClaimType = ClaimTypes.NameIdentifier,
+        RoleClaimType = "role",
+    };
+});
+builder.Services.AddAuthorization();
 
-await host.StartAsync();
+var app = builder.Build();
 
-var stepWiseClient = host.Services.GetRequiredService<StepWiseClient>();
+// enable cors from the same origin
+app.UseCors(builder =>
+{
+    builder.SetIsOriginAllowed(origin => origin.Contains("localhost"))
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+#pragma warning disable MVC1005 // Cannot use UseMvc with Endpoint Routing
+app.UseMvc();
+#pragma warning restore MVC1005 // Cannot use UseMvc with Endpoint Routing
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+await app.StartAsync();
+
+var stepWiseClient = app.Services.GetRequiredService<StepWiseClient>();
 
 var userInputWorkflow = Workflow.CreateFromInstance(new UserInput());
-var helloWorldWorkflow = Workflow.CreateFromInstance(new HelloWorld());
 var loopWorkflow = Workflow.CreateFromInstance(new Loop());
 var cumulativeWorkflow = Workflow.CreateFromInstance(new Cumulative());
 var basicSteps = new BasicSteps();
 var releaseMaster = new ReleaseMaster();
-var codeInterpreter = CodeInterpreter.Create();
 var ocrWorkflow = new OCR();
 var sequential = new Sequential();
 var ifElse = new IfElseBranching();
 var parallel = new ParallelWorkflow();
 var textToImage = new TextToImage();
-var imageClassification = new ImageClassification();
+var getWeather = new GetWeatherWorkflow();
+var checkScore = new CheckScore();
 
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(sequential));
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(ifElse));
 stepWiseClient.AddWorkflow(loopWorkflow);
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(parallel));
 stepWiseClient.AddWorkflow(userInputWorkflow);
-
-stepWiseClient.AddWorkflow(helloWorldWorkflow);
+stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(getWeather));
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(new PrepareDinner()));
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(releaseMaster));
-stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(codeInterpreter));
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(ocrWorkflow));
 stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(textToImage));
-stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(imageClassification));
+stepWiseClient.AddWorkflow(Workflow.CreateFromInstance(checkScore));
 
 // Wait for the host to shutdown
-await host.WaitForShutdownAsync();
+await app.WaitForShutdownAsync();
+
+
+
 
