8b64763b55fd836eb0462c4a6dc62d4b3eabec3c
XiaoYun Zhang <xiaoyuz@microsoft.com>
2025-03-30 00:41:17 -0700

    add MCP example

---
+++ example/MCP/WeatherChat/Client/Program.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) LittleLittleCloud. All rights reserved.
+// Program.cs
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Transport;
+using OpenAI;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Configuration
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>();
+
+var (command, arguments) = GetCommandAndArguments(args);
+
+await using var mcpClient = await McpClientFactory.CreateAsync(new()
+{
+    Id = "weather-chat-server",
+    Name = "Weather Server",
+    TransportType = TransportTypes.StdIo,
+    TransportOptions = new()
+    {
+        ["command"] = command,
+        ["arguments"] = arguments,
+    }
+});
+
+var tools = await mcpClient.ListToolsAsync();
+foreach (var tool in tools)
+{
+    Console.WriteLine($"Connected to server with tools: {tool.Name}");
+}
+
+var openAIApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new ArgumentNullException("OPENAI_API_KEY is not found");
+var model = "gpt-4o-mini";
+var openAIClient = new OpenAIClient(openAIApiKey);
+var chatClient = openAIClient.AsChatClient(model)
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build();
+
+var options = new ChatOptions
+{
+    Tools = [.. tools],
+};
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("MCP Client Started!");
+Console.ResetColor();
+
+PromptForInput();
+while (Console.ReadLine() is string query && !"exit".Equals(query, StringComparison.OrdinalIgnoreCase))
+{
+    if (string.IsNullOrWhiteSpace(query))
+    {
+        PromptForInput();
+        continue;
+    }
+
+    await foreach (var message in chatClient.GetStreamingResponseAsync(query, options))
+    {
+        Console.Write(message);
+    }
+    Console.WriteLine();
+
+    PromptForInput();
+}
+
+static void PromptForInput()
+{
+    Console.WriteLine("Enter a command (or 'exit' to quit):");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write("> ");
+    Console.ResetColor();
+}
+
+/// <summary>
+/// Determines the command (executable) to run and the script/path to pass to it. This allows different
+/// languages/runtime environments to be used as the MCP server.
+/// </summary>
+/// <remarks>
+/// This method uses the file extension of the first argument to determine the command, if it's py, it'll run python,
+/// if it's js, it'll run node, if it's a directory or a csproj file, it'll run dotnet.
+/// 
+/// If no arguments are provided, it defaults to running the QuickstartWeatherServer project from the current repo.
+/// 
+/// This method would only be required if you're creating a generic client, such as we use for the quickstart.
+/// </remarks>
+static (string command, string arguments) GetCommandAndArguments(string[] args)
+{
+    return args switch
+    {
+        [var script] when script.EndsWith(".py") => ("python", script),
+        [var script] when script.EndsWith(".js") => ("node", script),
+        [var script] when Directory.Exists(script) || (File.Exists(script) && script.EndsWith(".csproj")) => ("dotnet", $"run --project {script} --no-build"),
+        _ => ("dotnet", "run --project ../../../../WeatherServer --no-build")
+    };
+}
---
+++ example/MCP/WeatherChat/WeatherServer/Program.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) LittleLittleCloud. All rights reserved.
+// Program.cs
+
+using System.Text.Json;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol;
+using OpenAI;
+using StepWise.Core;
+using StepWise.Core.Extension;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+});
+
+var getWeather = new GetWeatherWorkflow();
+
+var workflow = Workflow.CreateFromInstance(getWeather);
+var engine = StepWiseEngine.CreateFromInstance(getWeather, loggerFactory.CreateLogger("workflow"));
+var tools = engine.GetAIFunctions();
+
+var chatOption = new ChatOptions
+{
+    Tools = [.. tools],
+};
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+    .AddMcpServer(option =>
+    {
+        option.Capabilities = new()
+        {
+            Tools = new ModelContextProtocol.Protocol.Types.ToolsCapability
+            {
+                ToolCollection = engine.GetMcpServerToolCollection(),
+            },
+        };
+    })
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+var app = builder.Build();
+
+await app.RunAsync();
+
+public class GetWeatherWorkflow
+{
+    [Step]
+    public async Task<string?> GetWeatherAsync(
+        string[] cities)
+    {
+        var date = DateTime.Now.ToString("yyyy-MM-dd");
+        var weathers = cities.Select(city => new Weather(city, date, "Sunny")).ToArray();
+
+        return JsonSerializer.Serialize(weathers);
+    }
+
+    public class Weather
+    {
+        public Weather(string city, string date, string forecast)
+        {
+            City = city;
+            Date = date;
+            Forecast = forecast;
+        }
+        public string City { get; }
+
+        public string Date { get; }
+
+        public string Forecast { get; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
